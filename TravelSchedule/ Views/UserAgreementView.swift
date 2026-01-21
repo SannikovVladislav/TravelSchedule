175fdcd05e0a86ffcda2dae89ae67ae506786258
SannikovVladislav <sannikovladislav@mail.ru>
2026-01-21 22:28:37 +0300

    Добавлен ВебВью

---
+++ TravelSchedule/ Views/UserAgreementView.swift
@@ -0,0 +1,18 @@
+//
+//  UserAgreementView.swift
+//  TravelSchedule
+//
+//  Created by Владислав on 21.01.2026.
+//
+
+import SwiftUI
+
+struct UserAgreementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserAgreementView()
+}
